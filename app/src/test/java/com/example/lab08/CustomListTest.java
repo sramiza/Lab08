b2932ed572a680b87c89f674818516323efa29cc
Syed Ramiz Abbas <syedramizabbas@gmail.com>
2026-03-19 12:12:42 +0500

    failing build

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,18 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+
+        list.addCity(calgary);
+
+        // should return true if city exists
+        assertTrue(list.hasCity(calgary));
+    }
 }
