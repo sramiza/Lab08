6dc9fdb2c13c8091ccee18362681a0e0e0ca7a51
Syed Ramiz Abbas <syedramizabbas@gmail.com>
2026-03-19 12:30:59 +0500

    implemented delete city

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -18,6 +18,9 @@ public class CustomList {
         return cities.contains(city);
     }
 
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
 }
